ae28fa677d13f45f3159d1d2bd901fe6e123aee6
Mia Zhou <miazhou@Mias-MacBook-Air.local>
2026-08-20 13:24:51 +0800

    Animate the gallery postcards; crop the generator watermark and border

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1787202101|992042';
+const CACHE_VERSION = '1787203489|1045065';
 /** @type {string} */
 const CACHE_PREFIX = 'Dear, Unknown-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
