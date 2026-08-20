18c8a35f2c60f87821253edbb08088123c332124
Mia Zhou <miazhou@Mias-MacBook-Air.local>
2026-08-20 15:50:09 +0800

    Portrait postcard layout, fewer notifications, season-aware navigation

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1787203489|1045065';
+const CACHE_VERSION = '1787212208|1467268';
 /** @type {string} */
 const CACHE_PREFIX = 'Dear, Unknown-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
