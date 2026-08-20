fbb99d69c65d2efe3babda59a167522a9fbcf3d3
Mia Zhou <miazhou@Mias-MacBook-Air.local>
2026-08-20 17:03:57 +0800

    Season one re-cut to five puzzles a city, sizes following their subjects

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1787214077|1048249';
+const CACHE_VERSION = '1787216636|1018937';
 /** @type {string} */
 const CACHE_PREFIX = 'Dear, Unknown-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
