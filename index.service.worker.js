80658acd99ea08a27c485b78d72a3c7799442a18
Mia Zhou <miazhou@Mias-MacBook-Air.local>
2026-08-20 11:57:06 +0800

    Quiet the web loading screen; request persistent storage

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1787195861|1046904';
+const CACHE_VERSION = '1787198217|972935';
 /** @type {string} */
 const CACHE_PREFIX = 'Dear, Unknown-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
