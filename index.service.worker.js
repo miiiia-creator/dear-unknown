33b057b27faac487b78b9d5b6c2705215958fc8d
Mia Zhou <miazhou@Mias-MacBook-Air.local>
2026-08-21 13:37:03 +0800

    The blank card is already a composition

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1787288037|987261';
+const CACHE_VERSION = '1787290622|1077941';
 /** @type {string} */
 const CACHE_PREFIX = 'Dear, Unknown-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
