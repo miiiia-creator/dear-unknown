6cf96ad3d6566fece14c0e6cb33a7ce015fd33fa
Mia Zhou <miazhou@Mias-MacBook-Air.local>
2026-08-21 16:51:12 +0800

    Season two complete

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1787301823|1087869';
+const CACHE_VERSION = '1787302271|1019448';
 /** @type {string} */
 const CACHE_PREFIX = 'Dear, Unknown-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
