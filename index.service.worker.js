60554094e37fdb8724a3c67f59468f09e306f589
Mia Zhou <miazhou@Mias-MacBook-Air.local>
2026-08-21 16:19:20 +0800

    Point the tour at a puzzle that exists

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1787300202|1069829';
+const CACHE_VERSION = '1787300359|1317974';
 /** @type {string} */
 const CACHE_PREFIX = 'Dear, Unknown-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
