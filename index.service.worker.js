66e0f6e73d615c3f328ba63c19afbf9d169cc1dc
Mia Zhou <miazhou@Mias-MacBook-Air.local>
2026-08-21 23:50:43 +0800

    a way to reach the far end of the game

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1787327348|1289848';
+const CACHE_VERSION = '1787327443|1031849';
 /** @type {string} */
 const CACHE_PREFIX = 'Dear, Unknown-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
