0518908c55a2ceff23c56a2f29bc2d5757bc0ec8
Mia Zhou <miazhou@Mias-MacBook-Air.local>
2026-08-21 15:38:45 +0800

    Paris in colour, and a way between seasons

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1787295777|1088787';
+const CACHE_VERSION = '1787297925|1066181';
 /** @type {string} */
 const CACHE_PREFIX = 'Dear, Unknown-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
