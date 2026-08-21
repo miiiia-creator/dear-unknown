90f5011333bd3468f2eae053bf68a5fd84617c3e
Mia Zhou <miazhou@Mias-MacBook-Air.local>
2026-08-21 23:49:08 +0800

    lighter type, no films, and the postcards stop moving

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1787323833|1056728';
+const CACHE_VERSION = '1787327348|1289848';
 /** @type {string} */
 const CACHE_PREFIX = 'Dear, Unknown-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
