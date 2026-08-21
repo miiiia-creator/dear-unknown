df1ef95656dd6442cf61c611d9b9fb83c2ab6538
Mia Zhou <miazhou@Mias-MacBook-Air.local>
2026-08-21 19:55:13 +0800

    six bugs, and los angeles

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1787310971|1077376';
+const CACHE_VERSION = '1787313312|1096952';
 /** @type {string} */
 const CACHE_PREFIX = 'Dear, Unknown-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
