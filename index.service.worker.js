4807807503d307f920ab62312d690316d7c32647
Mia Zhou <miazhou@Mias-MacBook-Air.local>
2026-08-24 16:54:14 +0800

    put the build on the page

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1787334523|947225';
+const CACHE_VERSION = '1787561654|900158';
 /** @type {string} */
 const CACHE_PREFIX = 'Dear, Unknown-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
