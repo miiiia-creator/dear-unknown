f710e1b88b7db0c11e1b2be5ba24239f76263287
Mia Zhou <miazhou@Mias-MacBook-Air.local>
2026-08-27 15:48:09 +0800

    Two grids that teach, a letter that changes in front of you, and seven destinations

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1787729998|934826';
+const CACHE_VERSION = '1787816888|1033215';
 /** @type {string} */
 const CACHE_PREFIX = 'Dear, Unknown-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
